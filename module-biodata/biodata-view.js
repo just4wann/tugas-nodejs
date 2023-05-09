25ed590cf71ca833ce294227329443c1d7dc4fb2
Juan Fahrul Fauzan <juanfhrul08@gmail.com>
2023-05-09 20:04:59 +0700

    menambahkan folder module-biodata untuk pengumpulan tugas nodejs

---
+++ module-biodata/biodata-view.js
@@ -0,0 +1,6 @@
+const biodata = require('./biodata');
+
+console.log(biodata.data.name);
+console.log(biodata.data.placeOfBirth);
+console.log(biodata.data.dateOfBirth);
+console.log(biodata.data.address);
---
+++ module-biodata/biodata.js
@@ -0,0 +1,9 @@
+exports.data = {
+    name : 'Nama : Juan Fahrul Fauzan',
+    
+    placeOfBirth : 'Tempat Lahir : Tasikmalaya',
+    
+    dateOfBirth : 'Tanggal Lahir : 08 Juli 2003',
+    
+    address : 'Alamat : Cikarang Selatan, Bekasi'
+}
